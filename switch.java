90d1876077100566c8591331619d7800e9961976
Jayanth3003 <104014571+Jayanth3003@users.noreply.github.com>
2022-04-29 22:05:03 +0530

    Update and rename README.md to switch.java

---
+++ switch.java
@@ -0,0 +1,30 @@
+import java.util.*
+public class switch{
+public static void main(String args[]){
+int days;
+days=4;
+switch (days){
+case 1:
+System.out.println("monday");
+break;
+case 2:
+System.out.println("tuesday");
+break;
+case 3:
+System.out.println("wednesday");
+break;
+case 4:
+System.out.println("thursday");
+break;
+case 5:
+System.out.println("friday");
+break;
+case 6:
+System.out.println("saturday");
+break;
+case 7:
+System.out.println("sunday");
+break;
+}
+}
+}
